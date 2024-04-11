2f32273ccba648d54630b60720f314612054cc4f
Nihar Devireddy <nihardevireddy@gmail.com>
2024-04-11 19:13:08 -0400

    added decoder function

--- zml_lab9.py
+++ zml_lab9.py
@@ -1,13 +1,15 @@
 def main():
     while True:
-        print("Menu\n-------------\n1. Encode\n2. Decode\n3. Quit")
-        option = input(f"Please enter an option: ")
-        option = int(option)
+        print("\nMenu\n-------------\n1. Encode\n2. Decode\n3. Quit")
+        option = int(input(f"Please enter an option: "))
         if option == 1:
             message_to_encode = input("Please enter your password to encode: ")
             message_to_encode = int(message_to_encode)
-            encoded_string = encoder(message_to_encode)
-            print("Your password has been encoded and stored!")
+            encoded_string = str(encoder(message_to_encode))
+            print("Your password has been encoded and stored!") 
+        if option == 2:
+            decode_pass = decoder(encoded_string)
+            print(f"The encoded password is {encoded_string}, and the original password is {decode_pass}.")
         if option == 3:
             break
 
@@ -17,6 +19,13 @@ def encoder(password):
         character = int(character) +3
         new_password *= 10
         new_password += character
+    return new_password
+
+def decoder(encoded_password):
+    new_password = ""
+    for char in encoded_password:
+        char = int(char) - 3
+        new_password += str(char)
     return new_password
 
 
